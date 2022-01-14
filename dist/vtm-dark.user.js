6c376bb9309adcfddb6b7a996ecdaf3d2fd77f88
kookiekookie <74433656+kookiekookie@users.noreply.github.com>
2022-01-14 07:45:23 -0500

    Update vtm-dark.user.js

--- dist/vtm-dark.user.js
+++ dist/vtm-dark.user.js
@@ -7,7 +7,7 @@
 // @include       https://app.roll20.net/editor*
 // @include       https://app.roll20.net/campaigns/chatarchive*
 // @run-at        document-start
-// @version       2021.01.14.1
+// @version       2021.01.14.2
 // @license       GPL-3.0-or-later
 // Edit background image at line 460
 // Testing update
